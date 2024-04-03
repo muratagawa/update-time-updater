4d636d5e684f7b5a3b1c9f8ab62e2798ea41f15c
MURATAGAWA Kei <muratagawa@gmail.com>
2024-04-03 16:19:59 +0900

    Delete status bar message

--- main.ts
+++ main.ts
@@ -64,10 +64,6 @@ export default class UpdateTimeUpdaterPlugin extends Plugin {
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
 			id: 'open-sample-modal-simple',
