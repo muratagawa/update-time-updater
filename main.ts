5f80bc131fda72aa1b928ea87b59370a70a80a40
MURATAGAWA Kei <muratagawa@gmail.com>
2024-04-03 22:26:26 +0900

    Implement console command

--- main.ts
+++ main.ts
@@ -23,7 +23,7 @@ class UpdateTimeUpdaterSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Update Time Updater Settings'});
+		containerEl.createEl('h2', { text: 'Update Time Updater Settings' });
 
 		new Setting(containerEl)
 			.setName('Target key')
@@ -59,17 +59,7 @@ export default class UpdateTimeUpdaterPlugin extends Plugin {
 		// This creates an icon in the left ribbon.
 		const ribbonIconEl = this.addRibbonIcon('timer', 'Update modified date', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			const now = moment();
-			const f = this.app.workspace.getActiveFile();
-			if (f) {
-				let frontmatter = this.app.metadataCache.getFileCache(f)?.frontmatter;
-				if (frontmatter) {
-					this.app.fileManager.processFrontMatter(f, frontmatter => {
-						frontmatter[this.settings.updateKey] = now.format(this.settings.datetimeFormat);
-					});
-				}
-			}
-			new Notice(`Value of '${this.settings.updateKey}' has been updated.`);
+			this.updateFrontmatter();
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
@@ -79,48 +69,12 @@ export default class UpdateTimeUpdaterPlugin extends Plugin {
 			id: 'update-modified-datetime-manually',
 			name: 'Update manually',
 			callback: () => {
-				const now = moment();
-				new Notice(now.toString());
-				// new UpdateTimeUpdaterModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new UpdateTimeUpdaterModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+				this.updateFrontmatter();
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new UpdateTimeUpdaterSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-		// 	console.log('click', evt);
-		// });
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -138,10 +92,20 @@ export default class UpdateTimeUpdaterPlugin extends Plugin {
 		await this.saveData(this.settings);
 	}
 
-	async updateFrontmatter (file: TFile) {
-		//TODO
+	async updateFrontmatter() {
+		const now = moment();
+		const f = this.app.workspace.getActiveFile();
+		if (f) {
+			let frontmatter = this.app.metadataCache.getFileCache(f)?.frontmatter;
+			if (frontmatter) {
+				this.app.fileManager.processFrontMatter(f, frontmatter => {
+					frontmatter[this.settings.updateKey] = now.format(this.settings.datetimeFormat);
+				});
+			}
+		}
+		new Notice(`Value of '${this.settings.updateKey}' has been updated.`);
 	}
-	
+
 }
 
 
